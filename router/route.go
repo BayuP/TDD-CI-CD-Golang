91576603b4a591058c8bfbe671d7fbfc60cf7259
bayu yunianto pabisa <pabisajunior@gmail.com>
2020-11-19 21:27:18 +0700

    testing wo db

--- router/route.go
+++ router/route.go
@@ -1,9 +1,7 @@
 package router
 
 import (
-	"ecommerce/common"
 	"ecommerce/router/routes"
-	"log"
 
 	"github.com/gorilla/mux"
 )
@@ -11,9 +9,9 @@ import (
 func New() *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
 
-	err := common.Connect()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := common.Connect()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	return routes.SetupRouteWithMiddleware(r)
 }
